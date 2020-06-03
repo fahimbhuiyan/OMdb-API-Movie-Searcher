043dd27ebf858c520a0787404492b9e59f4b4860
fahimbhuiyan <fahimrbhuiyan@gmail.com>
2020-06-03 00:00:02 -0400

    Update App.js

--- OMDb/omdb/src/App.js
+++ OMDb/omdb/src/App.js
@@ -39,6 +39,35 @@ const SearchBox = ({searchHandler}) => {
     )
 }
 
+
+
+const MovieDetail = ({Title, Poster, Plot}) => {
+    return (
+        <Row>
+            <Col span={11}>
+                <img 
+                    src={Poster === 'N/A' ? 'https://placehold.it/198x264&text=Image+Not+Found' : Poster} 
+                    alt={Title} 
+                />
+            </Col>
+            <Col span={13}>
+                <Row>
+                    <Col span={21}>
+                        <TextTitle level={4}>{Title}</TextTitle></Col>
+                    <Col span={3} style={{textAlign:'right'}}>
+                        <TextTitle level={4}><span style={{color: '#41A8F8'}}></span></TextTitle>
+                    </Col>
+                </Row>
+                <Row style={{marginBottom: '20px'}}>
+                </Row>
+                <Row>
+                    <Col>{Plot}</Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
+
 const ColCardBox = ({Title, imdbID, Poster, Type, ShowDetail, DetailRequest, ActivateModal}) => {
 
     const clickHandler = () => {
@@ -81,33 +110,6 @@ const ColCardBox = ({Title, imdbID, Poster, Type, ShowDetail, DetailRequest, Act
                 </Card>
             </div>
         </Col>
-    )
-}
-
-const MovieDetail = ({Title, Poster, Plot}) => {
-    return (
-        <Row>
-            <Col span={11}>
-                <img 
-                    src={Poster === 'N/A' ? 'https://placehold.it/198x264&text=Image+Not+Found' : Poster} 
-                    alt={Title} 
-                />
-            </Col>
-            <Col span={13}>
-                <Row>
-                    <Col span={21}>
-                        <TextTitle level={4}>{Title}</TextTitle></Col>
-                    <Col span={3} style={{textAlign:'right'}}>
-                        <TextTitle level={4}><span style={{color: '#41A8F8'}}></span></TextTitle>
-                    </Col>
-                </Row>
-                <Row style={{marginBottom: '20px'}}>
-                </Row>
-                <Row>
-                    <Col>{Plot}</Col>
-                </Row>
-            </Col>
-        </Row>
     )
 }
 
